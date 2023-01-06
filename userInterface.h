69dbb253e36821ea9f4b98d7568935264609b0a1
Svetlana Kharchenko <durnfogus@gmail.com>
2023-01-07 00:31:50 +0500

    Добавила интерфейс пользователя

--- userInterface.h
+++ userInterface.h
@@ -4,5 +4,10 @@
 
 #ifndef PROJECT_NUMBER_3_USERINTERFACE_H
 #define PROJECT_NUMBER_3_USERINTERFACE_H
+#include "authorization.h"
+
+
+user_data check_if_admin(int user_data);
+void add_film();
 
 #endif //PROJECT_NUMBER_3_USERINTERFACE_H
