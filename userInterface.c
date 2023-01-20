40fb67d3bd67612732eff6e9d36d1851183604a0
Rufus <vladimerrybalkin@gmail.com>
2023-01-20 20:04:45 +1000

    Авторизация

--- userInterface.c
+++ userInterface.c
@@ -10,21 +10,54 @@
 
 #define skip fgets(temp, 27, USER_DATA)
 
-int check_if_admin(int is_admin){
+int check_if_admin(int is_admin) {
 
-    if( is_admin == 0){
-        puts("Введите 1, чтобы перейти в каталог\nВведите 2, чтобы перейти в избранное");
+    int check;
+    while(1){
+        system("cls");
+        if (is_admin == 0) {
+            puts("Введите 1, чтобы перейти в каталог\nВведите 2, чтобы перейти в избранное");
+        }
+        if (is_admin == 1) {
+            puts("Введите 1, чтобы перейти в каталог\nВведите 2, чтобы перейти в избранное\nВведите 3, чтобы перейти в добавление");
+        }
+
+        scanf("%d", &check); fflush(stdin);
+        if (check == 1 || check == 2 || (check == 3 && is_admin == 1)){
+            break;
+        }
     }
-    if( is_admin == 1){
-        puts("Введите 1, чтобы перейти в каталог\nВведите 2, чтобы перейти в избранное\nВведите 3, чтобы перейти в добавление");
-    }
-
-    wait();
-
+    return check;
 }
 
-void add_film(){
 
+void add_film(){
+    FILE *FILMS = fopen("../films.txt", "r");
+    char temp[100]; char str[100]; int flag = 0;
+    printf("Введите фильм\n");
+    SetConsoleCP(1251);
+    gets(str); fflush(stdin);
+    SetConsoleCP(866);
+    while(fgets(temp, 100, FILMS) != NULL){
+        if(strcmp(temp, str) == 0){
+            flag = 1;
+
+        }
+        else (flag = 0);
+    }
+
+    if(flag == 1){
+        printf("Фильм уже добавлен");
+    }
+    else{
+        fclose(FILMS);
+
+        FILE *FILM = fopen("../films.txt", "a");
+        printf("Добавь меня полностью папочка\n");
+        fprintf(FILM, "\n%s", str); fflush(stdin);
+    }
+    fclose(FILMS);
+    wait();
 };
 
 
