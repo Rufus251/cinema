877c915731ac8c145f97af3f6d1b417e460c6d31
Rufus <119047983+Rufus251@users.noreply.github.com>
2023-01-20 21:27:22 +1000

    Update catalog.c

--- catalog.c
+++ catalog.c
@@ -1,94 +1,80 @@
-#include "userInterface.h"
-#include "catalog.h"
-#include "addFilm.h"
-#include "authorization.h"
-
-#include <malloc.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <windows.h>
+#include "addFilm.h"
+#include "authorization.h"
+#include "catalog.h"
+#include "favorites.h"
+#include "registration.h"
+#include "userInterface.h"
+#include "functions.h" // Кидайте туда полезные штуки которые не вписываются в другие либы
 
-
-
-list scan(FILE *in){
-    list l; l.size = 0; l.head = NULL; l.tail = NULL;
-
-    char temp[100];
-    while (fgets(temp, 100, in) != NULL) {
-
-        catalog *c = (catalog *)malloc(sizeof(catalog));
-        temp[strcspn(temp, "\n")] = 0;
-        strcpy(c->name, temp);
-
-        fgets(temp, 100, in);
-        temp[strcspn(temp, "\n")] = 0;
-        c->year = atoi(temp);
-
-        fgets(temp, 100, in);
-        temp[strcspn(temp, "\n")] = 0;
-        strcpy(c->country, temp);
-
-        fgets(temp, 100, in);
-        temp[strcspn(temp, "\n")] = 0;
-        strcpy(c->genre, temp);
-
-        fgets(temp, 100, in);
-        temp[strcspn(temp, "\n")] = 0;
-        c->rating = atof(temp);
-
-        pushback(&l, c);
-    }
-    return l;
+void MishaCheck() {
+    char card[20]; //4111111111100031
+    scanf("%s", card); fflush(stdin);
+    printf("\n%d", another_check(card));
+    wait();
 }
 
 
-void pushback(list *l, catalog *c) {
-    if (l->size > 0) {
-        c->next = l->tail;
-        c->prev = l->head;
-        l->head->prev = c;
-        l->tail->next = c;
-        l->tail = c;
-        l->size++;
-    } else {
-        l->head = c;
-        l->tail = c;
-        c->next = c;
-        c->prev = c;
-        l->size++;
-    }
-}
-
-
-/*void filmprint(catalog *c) {
-    printf("%s%d\n%s%s%0.1f\n\n", c->name, c->year, c->country, c->genre, c->rating);
-}*/
-
-void print_list() {
+void VasyaCheck(){
     FILE *films = fopen("../films.txt", "r");
     list whole = scan(films);
-    catalog *current = whole.head;
-    int i = 0;
-    while (i < whole.size) {
-        print_film(current);
-        current = current->next;
-        i++;
-    }
+    print_list(whole);
     fclose(films);
+
+
+
+    /*while (1) {
+        printLeftRight(new -> prev -> card);
+        printDitailedCurrent(new -> card);
+        printLeftRight(new -> next -> card);
+    }*/
+
+
+    scanf(" ");
+    //Здесь заканчивается создание списка фильмов
 }
 
-void print_film(catalog *c) {
-    printf("                                 ┌──────────────────────────────────────────────┐\n");
-    printf("┌────────────────────────────────┘                                              └────────────────────────────────┐\n");
-    printf("                                                  %s\n", c->name);
-    printf("    %s                                                                           %s", c->prev->name, c->next->name);
-    printf("                                                                  %s      \n", c->genre);
-    printf("    %s                                                                           %s", c->prev->genre, c->next->genre);
-    printf("                                                                  %s      \n", c->country);
-    printf("    %s                                                                           %s\n", c->prev->country, c->next->country);
-    printf("                                                      %.1f+                     \n", c->rating);
-    printf("    %.1f+                                                                                      %.1f+ \n", c->prev->rating, c->next->rating);
-    printf("                                                    %d                     \n", c->year);
-    printf("└────────────────────────────────┐                                              ┌────────────────────────────────┘\n");
-    printf("                                 └──────────────────────────────────────────────┘\n");
+void SvetaCheck(){
+
+}
+
+int main()
+{
+    system("chcp 65001"); system("cls"); // Переключает консоль на русскую кодировку и очищает её,
+    // не убирайте 1 строку из мейна
+    //MishaCheck();
+    //return 0;
+
+    int is_admin = start();
+    int catalog_choice = check_if_admin(is_admin);
+    switch (catalog_choice) {
+        case 1:
+            printf("1");
+            wait();
+            break;
+
+        case 2:
+
+            printf("2");
+            wait();
+            break;
+        case 3:
+
+            add_film();
+            wait();
+            break;
+
+        default: return 0;
+    }
+    return 0;
+    /*
+    system("chcp 65001"); system("cls");
+
+    VasyaCheck();
+
+
+    return 0;
+    */
 }
