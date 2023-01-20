f3bb3aab18060c47aafa15a9e49d854029b25da3
Svetlana Kharchenko <durnfogus@gmail.com>
2023-01-20 19:53:47 +0500

    ааааааааааааааааааааааааааааааааааа

--- userInterface.c
+++ userInterface.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
-#include <windows.h>
-
-
 #include "userInterface.h"
-#include "favorites.h"
-#include "catalog.h"
-#include "addFilm.h"
 #include "authorization.h"
 
 
-int check_if_admin(int is_admin){
-
-    if( is_admin == 0){
+int check_if_admin(int is_admin) {
+    int check;
+    if (is_admin == 0) {
         puts("Введите 1, чтобы перейти в каталог\nВведите 2, чтобы перейти в избранное");
     }
-    if( is_admin == 1){
+    if (is_admin == 1) {
         puts("Введите 1, чтобы перейти в каталог\nВведите 2, чтобы перейти в избранное\nВведите 3, чтобы перейти в добавление");
     }
-
-    wait();
-
+    while(check != 1 || check != 2 || (check != 3 && is_admin == 1)){
+        scanf("%d", &check); fflush(stdin);
+    }
+    return check;
 }
 
-void add_film(){
-
-};
